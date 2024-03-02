c68bb496387d4cf00f1ae9f58fcaec9127db6325
Xwilarg <xwilarg@protonmail.com>
2024-03-02 15:06:45 +0100

    Add loot locker SDK

---
+++ Assets/Scripts/LootLockerManager.cs
@@ -0,0 +1,23 @@
+﻿using LootLocker.Requests;
+using UnityEngine;
+
+namespace SpeedJam4
+{
+    public class LootLockerManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            LootLockerSDKManager.StartGuestSession((response) =>
+            {
+                if (!response.success)
+                {
+                    Debug.Log($"Error starting LootLocker session: {response.text}");
+
+                    return;
+                }
+
+                Debug.Log("Successfully started LootLocker session");
+            });
+        }
+    }
+}
