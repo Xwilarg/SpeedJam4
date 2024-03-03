dbe397986751b7e3e5739513a08c28e2b45096ab
Xwilarg <xwilarg@protonmail.com>
2024-03-03 12:42:05 +0100

    Move input management to separate obj

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace SpeedJam4.Player
+{
+    public class InputManager : MonoBehaviour
+    {
+        public void OnRestart(InputAction.CallbackContext value)
+        {
+            if (value.performed)
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+
+        public void OnFire(InputAction.CallbackContext value)
+        {
+            if (PlayerController.Instance != null)
+            {
+                PlayerController.Instance.OnFire(value);
+            }
+        }
+    }
+}
